10ad8c00524b0a3635e37cb5cf89a9057550830d
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-20 08:34:55 -0800

    Add cart component and styles to HeaderCart.js

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,7 +1,23 @@
 import React from "react"
 
+import CartIcon from "../Cart/CartIcon"
+
+import classes from "./HeaderCart.module.css"
+
+
 const HeaderCart = () => {
-  
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+        </span>
+        <span>
+          Your Cart 
+          </span>
+          <span className={classes.badge}
+          >3 </span>
+      </button>
+  )
 }
 
 export default HeaderCart;
