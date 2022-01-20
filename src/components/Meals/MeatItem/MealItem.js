ac31787d06b3e762d51043f2e10b96774ab0a60b
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-20 10:18:48 -0800

    Add class names to MealItem component

--- src/components/Meals/MeatItem/MealItem.js
+++ src/components/Meals/MeatItem/MealItem.js
@@ -1,12 +1,14 @@
+import classes from "./MealItem.module.css"
+
 const MealItem = ({name, description, price}) => {
 
   const mealPrice = `$${price.toFixed(2)}`
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
         <h3>{name}</h3>
-        <div>{description}</div>
-        <div>{mealPrice}</div>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{mealPrice}</div>
       </div>
       <div>
 
