84c8f16f0701a92e8f4b0f0dde7b8fb245b6437c
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-30 10:22:09 -0800

    update reducer function to update item amount in cart if it already exists

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,7 +14,9 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id = {}
+  const cartItemRemoveHandler = (id) => {}
+
+  const cartItemAddHandler = (item) => {}
 
 
   const cartItems = (
@@ -22,7 +24,8 @@ const Cart = props => {
       {cartCtx.items.map((item) =>
     (
       <CartItem key={item.id} name={item.name} amount={item.amount} 
-      price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)}/>
+      price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)}
+      onAdd={cartItemAddHandler.bind(null, item)}/>
     ))}
     </ul>
     );
