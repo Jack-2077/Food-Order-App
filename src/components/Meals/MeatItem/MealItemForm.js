c5f9170c820aab9abaaecfdb0abf9f6e5055ddd4
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-30 08:20:08 -0800

    Add state to manage item amount

--- src/components/Meals/MeatItem/MealItemForm.js
+++ src/components/Meals/MeatItem/MealItemForm.js
@@ -1,4 +1,4 @@
-import {useRef} from "react"
+import {useRef, useState} from "react"
 
 import classes from "./MealItemForm.module.css"
 
@@ -6,17 +6,28 @@ import Input from "../../UI/Input"
 
 const MealItem = (props) => {
 
-  const amoutRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountRef = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
+    const amountInputRef = amountRef.current.value;
+    const amountNumber = +amountInputRef;
+
+    if(amountInputRef.trim().length === 0 || amountNumber < 1 || amountNumber > 5){
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(amountNumber);
   }
 
     return (
       <form className={classes.form} onSubmit={submitHandler} >
         <Input 
         label="Amount" 
-        ref={amoutRef}
+        ref={amountRef}
         input={{
           id: 'amount_' + props.id, 
           type: 'number',
@@ -26,6 +37,7 @@ const MealItem = (props) => {
           defaultValue: '1'
            }}/>
         <button>+ Add</button>
+        {!amountIsValid && <p>Please Enter a valid amount</p>}
         </form>
     )
 }
