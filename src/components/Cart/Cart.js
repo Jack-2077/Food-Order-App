d6bda24dba6982b3217f1c3d2bb64607abf803be
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-30 10:39:07 -0800

    update remove handler function in Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,7 +14,7 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {}
+  const cartItemRemoveHandler = (id) => { cartCtx.removeItem(id)}
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount: 1})
