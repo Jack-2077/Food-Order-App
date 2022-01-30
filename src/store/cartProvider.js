d6014509704efa8b4fa5d66e20b992df80a48f8d
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-30 10:37:30 -0800

    Add reducer function for removing cart item

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -29,6 +29,26 @@ const cartReducer = (state, action) => {
     return {items: updatedItems, 
       totalAmount: updatedTotalAmount};
   }
+
+  if(action.type === 'REMOVE_ITEM'){
+    const isItemExistsIndex = state.items.findIndex(item => item.id === action.id)
+    const existingCartItem = state.items[isItemExistsIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+
+    if(existingCartItem.amount === 1){
+       updatedItems =  state.items.filter(item => item.id !== action.id)
+    }else{
+        const updatedItem = {...existingCartItem, amount: existingCartItem.amount - 1};
+        updatedItems = [...state.items];
+        updatedItems[isItemExistsIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
   return defaultCartState;
 }
 
