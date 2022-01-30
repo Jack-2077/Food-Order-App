ff837c92441e8564b49ec8d72a8769332999c330
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-30 09:27:24 -0800

    Add remove item handler for cart item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,11 +14,15 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = id = {}
+
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) =>
     (
-      <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} />
+      <CartItem key={item.id} name={item.name} amount={item.amount} 
+      price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)}/>
     ))}
     </ul>
     );
