316c1489357715e021e9ba67fef693f65cbec0f6
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-11 20:36:29 -0800

    Add rect-hook-form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,24 +1,34 @@
+import {useForm} from "react-hook-form"
+
 import classes from "./Checkout.module.css"
+
 
 const Checkout = (props) => {
 
+  const {register, handleSubmit} = useForm()
+
+  const onSubmitHandler = (e) => {
+    //e.preventDefault();
+    alert(JSON.stringify(e))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
-        <input type="text" id='name' />
+        <input type="text" {...register("name", { required: true})} id='name' />
         </div>
         <div className={classes.control}>
         <label htmlFor='street'>Street</label>
-        <input type="text" id='street' />
+        <input type="text" {...register("street", { required: true})} id='street' />
         </div>
         <div className={classes.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type="text" id='postal' />
+        <input type="text" {...register("postal", { required: true})} id='postal' />
         </div>
         <div className={classes.control}>
         <label htmlFor='city'>City</label>
-        <input type="text" id='city' />
+        <input type="text" {...register("city", { required: true})} id='city' />
         </div>
         <button type="button" onClick={props.onCancel}>cancel</button>
         <button>Confirm</button>
