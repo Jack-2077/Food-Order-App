26a05ccb59e7a590f20540221380b5c5c52cfdc2
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-11 20:43:17 -0800

    Add error validation from react-hook-form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -5,7 +5,7 @@ import classes from "./Checkout.module.css"
 
 const Checkout = (props) => {
 
-  const {register, handleSubmit} = useForm()
+  const {register, formState: { errors }, handleSubmit} = useForm()
 
   const onSubmitHandler = (e) => {
     //e.preventDefault();
@@ -17,18 +17,22 @@ const Checkout = (props) => {
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
         <input type="text" {...register("name", { required: true})} id='name' />
+        {errors.name?.type === 'required' && "Name is required"}
         </div>
         <div className={classes.control}>
         <label htmlFor='street'>Street</label>
         <input type="text" {...register("street", { required: true})} id='street' />
+        {errors.street?.type === 'required' && "Street is required"}
         </div>
         <div className={classes.control}>
         <label htmlFor='postal'>Postal Code</label>
         <input type="text" {...register("postal", { required: true})} id='postal' />
+        {errors.postal?.type === 'required' && "Postal is required"}
         </div>
         <div className={classes.control}>
         <label htmlFor='city'>City</label>
         <input type="text" {...register("city", { required: true})} id='city' />
+        {errors.city?.type === 'required' && "City is required"}
         </div>
         <button type="button" onClick={props.onCancel}>cancel</button>
         <button>Confirm</button>
