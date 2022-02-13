1d3dd5c3e7d57053c3fb87386845172ec43f760e
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-12 19:31:05 -0800

    Add styles for errors in form validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -12,30 +12,37 @@ const Checkout = (props) => {
     alert(JSON.stringify(e))
   }
 
+  const isNameValid = errors.name?.type === 'required';
+  const isStreetValid = errors.street?.type === 'required';
+  const isPostalValid = errors.postal?.type === 'required';
+  const isCityValid = errors.postal?.type === 'required';
+
   return (
     <form onSubmit={handleSubmit(onSubmitHandler)}>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${!isNameValid ? '' : classes.invalid }`}>
         <label htmlFor='name'>Your Name</label>
         <input type="text" {...register("name", { required: true})} id='name' />
-        {errors.name?.type === 'required' && "Name is required"}
+        {isNameValid && "Name is required"}
         </div>
-        <div className={classes.control}>
+        <div className={`${classes.control} ${!isStreetValid ? '' : classes.invalid }`}>
         <label htmlFor='street'>Street</label>
         <input type="text" {...register("street", { required: true})} id='street' />
         {errors.street?.type === 'required' && "Street is required"}
         </div>
-        <div className={classes.control}>
+        <div className={`${classes.control} ${!isPostalValid ? '' : classes.invalid }`}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type="text" {...register("postal", { required: true})} id='postal' />
+        <input type="text" {...register("postal", { required: true,  maxLength: 6})} id='postal' />
         {errors.postal?.type === 'required' && "Postal is required"}
         </div>
-        <div className={classes.control}>
+        <div className={`${classes.control} ${!isCityValid ? '' : classes.invalid }`}>
         <label htmlFor='city'>City</label>
         <input type="text" {...register("city", { required: true})} id='city' />
-        {errors.city?.type === 'required' && "City is required"}
+        {errors.postal?.type === 'required' && "City is required"}
         </div>
+        <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>cancel</button>
         <button>Confirm</button>
+        </div>
       </form>
   )
 }
