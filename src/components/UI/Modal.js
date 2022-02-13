b601167a23150a45c6b5d48b905b0a2849f3fe22
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-13 10:34:44 -0800

    Fix image not showing up on home page

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -7,6 +7,8 @@ import classes from "./Modal.module.css"
 const Backdrop = props => {
   return <div className={classes.backdrop} onClick={props.onCartClose}/>
 }
+
+
 const ModalOverlay = props => {
 return (
   <div className={classes.modal}>
